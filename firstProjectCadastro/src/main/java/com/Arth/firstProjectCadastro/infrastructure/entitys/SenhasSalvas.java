1352062a77a57fb5540c5a701caf3a33ecea6566
Arthur Will <arthurwillyan0404fcl@gmail.com>
2026-09-08 20:56:17 -0300

    novos arquivos e funções criadas para a busca de senhas

--- firstProjectCadastro/src/main/java/com/Arth/firstProjectCadastro/infrastructure/entitys/SenhasSalvas.java
+++ firstProjectCadastro/src/main/java/com/Arth/firstProjectCadastro/infrastructure/entitys/SenhasSalvas.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 
-public class senhasSalvas {
+public class SenhasSalvas {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- firstProjectCadastro/src/main/java/com/Arth/firstProjectCadastro/infrastructure/repository/SenhasRepository.java
+++ firstProjectCadastro/src/main/java/com/Arth/firstProjectCadastro/infrastructure/repository/SenhasRepository.java
@@ -1,11 +1,11 @@
 package com.Arth.firstProjectCadastro.infrastructure.repository;
 
 import com.Arth.firstProjectCadastro.infrastructure.entitys.User;
-import com.Arth.firstProjectCadastro.infrastructure.entitys.senhasSalvas;
+import com.Arth.firstProjectCadastro.infrastructure.entitys.SenhasSalvas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface SenhasRepository extends JpaRepository<senhasSalvas, Integer> {
-    List<senhasSalvas> findByUsuario(User usuario);
+public interface SenhasRepository extends JpaRepository<SenhasSalvas, Integer> {
+    List<SenhasSalvas> findByUsuario(User usuario);
 }
